8ac1d3de796ab08eea723c93135003c8e6d1b73b
Raghav Rathi <ytwithrvr@gmail.com>
2024-02-07 16:04:32 +0530

    css exp3 : tried

---
+++ css_lab/exp3/playfair_cipher.java
@@ -0,0 +1,16 @@
+// import java.util.ArrayList;
+import java.util.Scanner;
+
+class exp3 {
+    public static void main(String ... args) {
+        System.out.println("Enter CipherText : ");
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        sc.close();
+
+        // ArrayList<Integer> splitText = new ArrayList<Integer>();
+
+
+        System.out.println("Text is : " + text);
+    }
+}
